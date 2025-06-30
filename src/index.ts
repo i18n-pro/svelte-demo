8a09924be3508ece42b2cdb8896023cfea9f871a
Eyelly Wu <eyelly.wu@gmail.com>
2025-06-30 22:29:48 +0800

    refactor(i18n): 重构国际化模块以提高性能和可维护性
    - 更新 i18n 库至最新版本并调整配置
    - 优化语言加载逻辑，支持动态加载
    - 重构 App 组件，简化国际化相关代码
    - 更新 tsconfig 以支持最新的 TypeScript 特性

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
+import {createI18n} from '@i18n-pro/svelte'
 import App from './App.svelte'
-import './i18n'
+import initI18nState from './i18n' 
 
-new App({
-  target: document.getElementById('app'),
-})
+
+async function init() {
+  const params = new URLSearchParams(
+    new URLSearchParams(window.location.search.slice(1)),
+  )
+  const locale = params.get('locale') || 'en'
+
+  const langs = initI18nState.langs || {}
+  if (locale !== 'zh') {
+    const lang = await (await fetch(`../i18n/${locale}.json`)).json()
+    langs[locale] = lang
+  }
+
+  createI18n({
+    ...initI18nState,
+    langs,
+    locale,
+  })
+
+  new App({
+    target: document.getElementById('app'),
+  })
+}
+
+init()
